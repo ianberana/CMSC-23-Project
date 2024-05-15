bc9d8883815d7d7e3faba5de005c35e0b7f63eda
jaEllyace <100139178+jaEllyace@users.noreply.github.com>
2024-05-15 21:53:05 +0800

    renamed signup.dart

--- lib/pages/authentication/signin.dart
+++ lib/pages/authentication/signin.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../providers/auth_provider.dart';
-import 'donor_signup.dart';
+import 'signup.dart';
 
 class SignInPage extends StatefulWidget {
   const SignInPage({super.key});
