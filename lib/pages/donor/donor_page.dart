64b8a8178332988ce618ab07a1290a37f6715c0f
jaEllyace <100139178+jaEllyace@users.noreply.github.com>
2024-05-30 03:16:05 +0800

    added donor form

--- lib/pages/donor/donor_page.dart
+++ lib/pages/donor/donor_page.dart
@@ -30,14 +30,14 @@ class _DonorPageState extends State<DonorPage> {
             title: Text("Red Cross Youth of UPLB"),
             subtitle: Text("The Red Cross Youth of University of the Philippines Los Banos is a non profit, mass based, student civic organization that aims to uphold humanity as one of its principles."),
             onTap: () {
-              // Add action when Organization 1 is tapped
+              _showDonateDialog(context);
             },
           ),
           ListTile(
-            title: Text("Umalohokan Inc."),
+            title: Text("Umalohokan, Inc."),
             subtitle: Text("Umalohokan, Inc. is a socio-cultural organization founded on December 21, 1977 in the University of the Philippines-Los Baños, at the height of the 1972 Martial Law."),
             onTap: () {
-              // Add action when Organization 2 is tapped
+              _showDonateDialog(context);
             },
           ),
           // Add more ListTile widgets for additional organizations
@@ -88,4 +88,87 @@ class _DonorPageState extends State<DonorPage> {
       return File(image.path);
     }
   }
+
+  void _showDonateDialog(BuildContext context) {
+  // Define the available donation items
+  List<String> donationItems = ["Food", "Clothes", "Cash"];
+
+  // Initialize a list to hold the selected items
+  List<String> selectedItems = [];
+
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      return SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                "Donate to Organization",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 20),
+              // Checkbox group for donation items
+              for (String item in donationItems)
+                CheckboxListTile(
+                  title: Text(item),
+                  value: selectedItems.contains(item),
+                  onChanged: (bool? value) {
+                    setState(() {
+                      if (value != null && value) {
+                        // If checkbox is checked, add item to selectedItems
+                        selectedItems.add(item);
+                      } else {
+                        // If checkbox is unchecked, remove item from selectedItems
+                        selectedItems.remove(item);
+                      }
+                    });
+                  },
+                ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Pickup or Drop off"),
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Weight of donation (kg)"),
+                keyboardType: TextInputType.number,
+              ),
+              SizedBox(height: 10),
+              TextFormField(
+                decoration: InputDecoration(labelText: "Date and Time of pickup/drop off"),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                //   Donation donation = Donation(
+                //   dateCreated: DateTime.now(),
+                //   item: "food",
+                //   delivery: "pickup",
+                //   weight: 20,
+                //   dateDelivery: DateTime.now(),
+                //   address: ["Los Banos, Laguna"],
+                //   contact: "09123456789",
+                //   donorId: "VLYloaQO4QwZS8Ve0ouE",
+                // );
+                  Navigator.pop(context);
+                },
+                child: Text("Submit"),
+              ),
+            ],
+          ),
+        ),
+      );
+    },
+  );
+}
+
+
 }
