476dff0c612ced0c2a675c2f088eb261e7329ea7
jaEllyace <100139178+jaEllyace@users.noreply.github.com>
2024-05-30 02:20:26 +0800

    added new drawer

--- lib/pages/donor/donor_page.dart
+++ lib/pages/donor/donor_page.dart
@@ -1,4 +1,5 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:elbi_donate/pages/donor/donor_drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../providers/auth_provider.dart';
@@ -14,131 +15,14 @@ class DonorPage extends StatefulWidget {
 class _DonorPageState extends State<DonorPage> {
   @override
   Widget build(BuildContext context) {
-    // Stream<QuerySnapshot> todosStream = context.watch<TodoListProvider>().todo;
     return Scaffold(
-        drawer: drawer,
+        drawer: DonorDrawer(),
         appBar: AppBar(
           title: const Text("Donors Page"),
         ),
         body: Container()
-        // StreamBuilder(
-        //   stream: todosStream,
-        //   builder: (context, snapshot) {
-        //     if (snapshot.hasError) {
-        //       return Center(
-        //         child: Text("Error encountered! ${snapshot.error}"),
-        //       );
-        //     } else if (snapshot.connectionState == ConnectionState.waiting) {
-        //       return const Center(
-        //         child: CircularProgressIndicator(),
-        //       );
-        //     } else if (!snapshot.hasData) {
-        //       return const Center(
-        //         child: Text("No Todos Found"),
-        //       );
-        //     }
-
-        //     return ListView.builder(
-        //       itemCount: snapshot.data?.docs.length,
-        //       itemBuilder: ((context, index) {
-        //         Todo todo = Todo.fromJson(
-        //             snapshot.data?.docs[index].data() as Map<String, dynamic>);
-        //         todo.id = snapshot.data?.docs[index].id;
-        //         return Dismissible(
-        //           key: Key(todo.id.toString()),
-        //           onDismissed: (direction) {
-        //             context.read<TodoListProvider>().deleteTodo(todo.title);
-
-        //             ScaffoldMessenger.of(context).showSnackBar(
-        //                 SnackBar(content: Text('${todo.title} dismissed')));
-        //           },
-        //           background: Container(
-        //             color: Colors.red,
-        //             child: const Icon(Icons.delete),
-        //           ),
-        //           child: ListTile(
-        //             title: Text(todo.title),
-        //             leading: Checkbox(
-        //               value: todo.completed,
-        //               onChanged: (bool? value) {
-        //                 context
-        //                     .read<TodoListProvider>()
-        //                     .toggleStatus(todo.id!, value!);
-        //               },
-        //             ),
-        //             trailing: Row(
-        //               mainAxisSize: MainAxisSize.min,
-        //               children: [
-        //                 IconButton(
-        //                   onPressed: () {
-        //                     showDialog(
-        //                       context: context,
-        //                       builder: (BuildContext context) => TodoModal(
-        //                         type: 'Edit',
-        //                         item: todo,
-        //                       ),
-        //                     );
-        //                   },
-        //                   icon: const Icon(Icons.create_outlined),
-        //                 ),
-        //                 IconButton(
-        //                   onPressed: () {
-        //                     showDialog(
-        //                       context: context,
-        //                       builder: (BuildContext context) => TodoModal(
-        //                         type: 'Delete',
-        //                         item: todo,
-        //                       ),
-        //                     );
-        //                   },
-        //                   icon: const Icon(Icons.delete_outlined),
-        //                 )
-        //               ],
-        //             ),
-        //           ),
-        //         );
-        //       }),
-        //     );
-        //   },
-        // ),
-        // floatingActionButton: FloatingActionButton(
-        //   onPressed: () {
-        //     showDialog(
-        //       context: context,
-        //       builder: (BuildContext context) => TodoModal(
-        //         type: 'Add',
-        //         item: null,
-        //       ),
-        //     );
-        //   },
-        //   child: const Icon(Icons.add_outlined),
-        // ),
+        
         );
   }
-
-  Drawer get drawer => Drawer(
-          child: ListView(padding: EdgeInsets.zero, children: [
-        const DrawerHeader(child: Text("Elbi Donate")),
-        ListTile(
-          title: const Text('Details'),
-          onTap: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const DonorProfile()));
-          },
-        ),
-        ListTile(
-          title: const Text('Donate'),
-          onTap: () {
-            // Navigator.pop(context);
-            // Navigator.pushNamed(context, "/");
-          },
-        ),
-        ListTile(
-          title: const Text('Logout'),
-          onTap: () {
-            context.read<UserAuthProvider>().signOut();
-            Navigator.pop(context);
-          },
-        ),
-      ]));
+  
 }
