0956e9fcbd8b208d2d7173b4e4a3d819f721e57d
Genmaa <mtalarcon1@up.edu.ph>
2024-05-29 20:57:35 +0800

    fixed design

--- lib/pages/organization/org_page.dart
+++ lib/pages/organization/org_page.dart
@@ -1,7 +1,11 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:elbi_donate/models/donation_model.dart';
+import 'package:elbi_donate/models/donor_model.dart';
 import 'package:elbi_donate/pages/organization/org_donationDetails.dart';
 import 'package:elbi_donate/pages/organization/org_donationDrive.dart';
 import 'package:elbi_donate/pages/organization/org_drawer.dart';
+import 'package:elbi_donate/providers/donor_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../providers/auth_provider.dart';
@@ -16,10 +20,11 @@ class OrganizationPage extends StatefulWidget {
 }
 
 class _OrganizationPageState extends State<OrganizationPage> {
+  String status = 'Pending';
   @override
   Widget build(BuildContext context) {
-    // Stream<QuerySnapshot> todosStream = context.watch<TodoListProvider>().todo;
-    String status = 'Pending';
+    Stream<QuerySnapshot> donorStream =
+        context.watch<DonorListProvider>().getDonor;
     return Scaffold(
       appBar: AppBar(
         title: Text('Donations',
@@ -32,312 +37,125 @@ class _OrganizationPageState extends State<OrganizationPage> {
       drawer: OrgDrawer(),
       body: Container(
         color: Color(0xFF008080),
-        child: ListView(
-          children: [
-            DonationCard(),
-            DonationCard(),
-          ],
-        ),
-      ),
-    );
-    // StreamBuilder(
-    //   stream: todosStream,
-    //   builder: (context, snapshot) {
-    //     if (snapshot.hasError) {
-    //       return Center(
-    //         child: Text("Error encountered! ${snapshot.error}"),
-    //       );
-    //     } else if (snapshot.connectionState == ConnectionState.waiting) {
-    //       return const Center(
-    //         child: CircularProgressIndicator(),
-    //       );
-    //     } else if (!snapshot.hasData) {
-    //       return const Center(
-    //         child: Text("No Todos Found"),
-    //       );
-    //     }
+        child: StreamBuilder(
+          stream: donorStream,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("Error encountered! ${snapshot.error}"),
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (!snapshot.hasData) {
+              return const Center(
+                child: Text("No Donations Yet"),
+              );
+            }
 
-    //     return ListView.builder(
-    //       itemCount: snapshot.data?.docs.length,
-    //       itemBuilder: ((context, index) {
-    //         Todo todo = Todo.fromJson(
-    //             snapshot.data?.docs[index].data() as Map<String, dynamic>);
-    //         todo.id = snapshot.data?.docs[index].id;
-    //         return Dismissible(
-    //           key: Key(todo.id.toString()),
-    //           onDismissed: (direction) {
-    //             context.read<TodoListProvider>().deleteTodo(todo.title);
+            return ListView.builder(
+              itemCount: snapshot.data?.docs.length,
+              itemBuilder: ((context, index) {
+                Donor donor = Donor.fromJson(
+                    snapshot.data?.docs[index].data() as Map<String, dynamic>);
+                donor.id = snapshot.data?.docs[index].id;
 
-    //             ScaffoldMessenger.of(context).showSnackBar(
-    //                 SnackBar(content: Text('${todo.title} dismissed')));
-    //           },
-    //           background: Container(
-    //             color: Colors.red,
-    //             child: const Icon(Icons.delete),
-    //           ),
-    //           child: ListTile(
-    //             title: Text(todo.title),
-    //             leading: Checkbox(
-    //               value: todo.completed,
-    //               onChanged: (bool? value) {
-    //                 context
-    //                     .read<TodoListProvider>()
-    //                     .toggleStatus(todo.id!, value!);
-    //               },
-    //             ),
-    //             trailing: Row(
-    //               mainAxisSize: MainAxisSize.min,
-    //               children: [
-    //                 IconButton(
-    //                   onPressed: () {
-    //                     showDialog(
-    //                       context: context,
-    //                       builder: (BuildContext context) => TodoModal(
-    //                         type: 'Edit',
-    //                         item: todo,
-    //                       ),
-    //                     );
-    //                   },
-    //                   icon: const Icon(Icons.create_outlined),
-    //                 ),
-    //                 IconButton(
-    //                   onPressed: () {
-    //                     showDialog(
-    //                       context: context,
-    //                       builder: (BuildContext context) => TodoModal(
-    //                         type: 'Delete',
-    //                         item: todo,
-    //                       ),
-    //                     );
-    //                   },
-    //                   icon: const Icon(Icons.delete_outlined),
-    //                 )
-    //               ],
-    //             ),
-    //           ),
-    //         );
-    //       }),
-    //     );
-    //   },
-    // ),
-    //   floatingActionButton: FloatingActionButton(
-    //     onPressed: () async {
-    //       // showDialog(
-    //       //   context: context,
-    //       //   builder: (BuildContext context) => TodoModal(
-    //       //     type: 'Add',
-    //       //     item: null,
-    //       //   ),
-    //       // );
-    //       await context
-    //           .read<DonationListProvider>()
-    //           .updateStatus("LJoJUvfjpIBNNd3KFWPG", "completed");
-
-    //       await context
-    //           .read<DonationListProvider>()
-    //           .updateDrive("LJoJUvfjpIBNNd3KFWPG", "LJoJUvfjpIBNNd3KFWPG");
-    //     },
-    //     child: const Icon(Icons.add_outlined),
-    //   ),
-    // );
-  }
-
-  // Drawer get drawer => Drawer(
-  //         child: ListView(padding: EdgeInsets.zero, children: [
-  //       const DrawerHeader(decoration: BoxDecoration(color: Colors.white),child: Text("Elbi Donate")),
-  //       ListTile(
-  //         title: const Text('Details'),
-  //         onTap: () {
-  //           Navigator.push(context,
-  //               MaterialPageRoute(builder: (context) => OrgDonationDetails()));
-  //         },
-  //       ),
-  //       ListTile(
-  //         title: const Text('Donation Drive'),
-  //         onTap: () {
-  //           Navigator.push(context,
-  //               MaterialPageRoute(builder: (context) => OrgDonationDrive()));
-  //           // Navigator.pop(context);
-  //           // Navigator.pushNamed(context, "/");
-  //         },
-  //       ),
-  //       ListTile(
-  //         title: const Text('Logout'),
-  //         onTap: () {
-  //           context.read<UserAuthProvider>().signOut();
-  //           Navigator.pop(context);
-  //         },
-  //       ),
-  //     ]));
-}
-
-// class DonationCard extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//       shape: RoundedRectangleBorder(
-//         borderRadius: BorderRadius.circular(10.0),
-//       ),
-//       child: Padding(
-//         padding: EdgeInsets.all(8.0),
-//         child: Column(
-//           crossAxisAlignment: CrossAxisAlignment.start,
-//           children: [
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//               children: [
-//                 Text(
-//                   'Magen Alarcon',
-//                   style: TextStyle(
-//                     fontWeight: FontWeight.bold,
-//                     fontSize: 18.0,
-//                   ),
-//                 ),
-//                 Row(
-//                   children: [
-//                     Text(
-//                       'Pending',
-//                       style: TextStyle(
-//                         color: Colors.teal,
-//                         fontWeight: FontWeight.bold,
-//                       ),
-//                     ),
-//                     Icon(
-//                       Icons.arrow_drop_down,
-//                       color: Colors.teal,
-//                     ),
-//                   ],
-//                 ),
-//               ],
-//             ),
-//             SizedBox(height: 8.0),
-//             Row(
-//               children: [
-//                 Icon(
-//                   Icons.access_time,
-//                   size: 16.0,
-//                 ),
-//                 SizedBox(width: 4.0),
-//                 Text('12-02-2020'),
-//               ],
-//             ),
-//             SizedBox(height: 8.0),
-//             Align(
-//               alignment: Alignment.centerRight,
-//               child: ElevatedButton(
-//                 onPressed: () {},
-//                 style: ElevatedButton.styleFrom(
-//                   backgroundColor: Color(0xFF008080),
-//                   shape: RoundedRectangleBorder(
-//                     borderRadius: BorderRadius.circular(10.0),
-//                   ),
-//                   padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-//                 ),
-//                 child: Text('Link Donation', style: TextStyle(color: Colors.white),),
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-
-//     );
-//   }
-// }
-
-class DonationCard extends StatefulWidget {
-  @override
-  _DonationCardState createState() => _DonationCardState();
-}
-
-class _DonationCardState extends State<DonationCard> {
-  String status = 'Pending';
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: IntrinsicHeight(
-        child: GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => OrgDonationDetails()),
-            );
-          },
-          child: Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'Magen Alarcon',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18.0,
-                        ),
-                      ),
-                      SizedBox(height: 8.0),
-                      Row(
+                return Dismissible(
+                  key: Key(donor.id.toString()),
+                  child: Card(
+                    margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16),
+                    child: ListTile(
+                      title: Row(
                         children: [
-                          Icon(
-                            Icons.access_time,
-                            size: 16.0,
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  '${donor.name}',
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                  ),
+                                ),
+                                SizedBox(height: 5.0),
+                                Row(
+                                  children: [
+                                    const Icon(
+                                      Icons.phone_callback,
+                                      size: 16.0,
+                                    ),
+                                    SizedBox(width: 4.0),
+                                    Text('${donor.contact}'),
+                                  ],
+                                ),
+                                SizedBox(height: 3.0),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.access_time,
+                                      size: 16.0,
+                                    ),
+                                    SizedBox(width: 4.0),
+                                    Text('12-02-2020'),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                          SizedBox(width: 4.0),
-                          Text('12-02-2020'),
+                          IntrinsicWidth(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                DropdownButton<String>(
+                                  value: status,
+                                  items: ['Pending', 'Accepted', 'Completed']
+                                      .map((status) {
+                                    return DropdownMenuItem<String>(
+                                      value: status,
+                                      child: Text(status),
+                                    );
+                                  }).toList(),
+                                  onChanged: (newStatus) {
+                                    setState(() {
+                                      status = newStatus!;
+                                    });
+                                  },
+                                ),
+                                ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color(0xFF008080),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 16.0, vertical: 8.0),
+                                  ),
+                                  child: Text(
+                                    'Link Donation',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
                         ],
                       ),
-                    ],
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => OrgDonationDetails()),
+                        );
+                      },
+                    ),
                   ),
-                  SizedBox(height: 8.0),
-                  Column(
-                    children: [
-                      DropdownButton<String>(
-                        value: status,
-                        items:
-                            ['Pending', 'Accepted', 'Completed'].map((status) {
-                          return DropdownMenuItem<String>(
-                            value: status,
-                            child: Text(status),
-                          );
-                        }).toList(),
-                        onChanged: (newStatus) {
-                          setState(() {
-                            status = newStatus!;
-                          });
-                        },
-                      ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Color(0xFF008080),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 16.0, vertical: 8.0),
-                          ),
-                          child: Text(
-                            'Link Donation',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ),
-          ),
+                );
+              }),
+            );
+          },
         ),
       ),
     );
